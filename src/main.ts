21ef499e51595eac48a99a785ff2718cd16f158c
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2023-10-16 13:22:43 +0530

    added CSRF Protection

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
+import * as csurf from 'csurf';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(csurf());
   app.use(
     helmet({
       crossOriginEmbedderPolicy: false,
