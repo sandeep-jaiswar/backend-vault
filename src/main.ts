79db8202ff0300e84807b240944e8ebf687d21a1
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2023-10-16 13:17:04 +0530

    added helmet to set security-related HTTP headers

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    helmet({
+      crossOriginEmbedderPolicy: false,
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: [`'self'`],
+          styleSrc: [`'self'`],
+          fontSrc: [`'self'`],
+          imgSrc: [`'self'`],
+          scriptSrc: [`'self'`],
+        },
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
